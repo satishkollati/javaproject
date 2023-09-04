42bb73874c83ded6e11ff9693d87d75b54303c23
satishkollati <101156460+satishkollati@users.noreply.github.com>
2023-09-04 15:53:14 +0530

    Update DemoApplication.java

--- src/main/java/com/satish/demo/DemoApplication.java
+++ src/main/java/com/satish/demo/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.satish.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
-
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Ss!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
